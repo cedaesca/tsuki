a206092e435dfc8814183ecfca1ebc1a0a131fca
Cesar Escudero <cedaesca@gmail.com>
2024-05-21 10:51:38 -0400

    test: cases for discord service

--- src/discord/discord.service.spec.ts
+++ src/discord/discord.service.spec.ts
@@ -1,9 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DiscordService } from './discord.service';
-import { Client } from 'discord.js';
+import {
+  Client,
+  CommandInteraction,
+  Events,
+  GatewayIntentBits,
+} from 'discord.js';
 import { ConfigService } from '@nestjs/config';
 import { ConsoleLogger } from '@nestjs/common';
 import { CommandsService } from '../commands/commands.service';
+import { Command } from 'src/commands/interfaces/command.interface';
+
+jest.mock('discord.js', () => {
+  const actualDiscordJs = jest.requireActual('discord.js');
+  return {
+    ...actualDiscordJs,
+    Client: jest.fn(() => ({
+      on: jest.fn(),
+      login: jest.fn(),
+      user: {
+        tag: 'test-bot-tag',
+      },
+      emit: jest.fn(),
+    })),
+  };
+});
 
 describe('DiscordService', () => {
   let service: DiscordService;
@@ -12,16 +33,22 @@ describe('DiscordService', () => {
   let logger: ConsoleLogger;
   let commandsService: CommandsService;
 
+  const getDiscordEventHandler = (eventName) => {
+    return client.on.mock.calls.find(([event]) => event === eventName)[1];
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         DiscordService,
         {
           provide: Client,
-          useValue: jest.fn(() => ({
-            on: jest.fn(),
-            login: jest.fn(),
-          })),
+          useValue: new Client({
+            intents: [
+              GatewayIntentBits.Guilds,
+              GatewayIntentBits.GuildMessages,
+            ],
+          }),
         },
         {
           provide: ConfigService,
@@ -52,9 +79,68 @@ describe('DiscordService', () => {
     configService = module.get<ConfigService>(ConfigService);
     logger = module.get<ConsoleLogger>(ConsoleLogger);
     commandsService = module.get<CommandsService>(CommandsService);
+
+    await service.init();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should login on init, using secret from env variables', async () => {
+    expect(client.login).toHaveBeenCalledWith('test-bot-token');
+    expect(configService.get).toHaveBeenCalledWith('BOT_SECRET');
+  });
+
+  it('should register a listener for commands interaction', async () => {
+    expect(client.on).toHaveBeenCalledWith(
+      Events.InteractionCreate,
+      expect.any(Function),
+    );
+  });
+
+  it('should not do anything if the interaction is not a command', async () => {
+    const interaction = {
+      isCommand: jest.fn().mockReturnValue(false),
+    } as unknown as CommandInteraction;
+
+    const interactionHandler = getDiscordEventHandler(Events.InteractionCreate);
+    await interactionHandler(interaction);
+
+    expect(interaction.isCommand).toHaveBeenCalled();
+    expect(commandsService.getCommand).not.toHaveBeenCalled();
+  });
+
+  it('should fetch and execute the command if it’s a valid one', async () => {
+    const executeSpy = jest.fn();
+
+    const interaction = {
+      isCommand: jest.fn().mockReturnValue(true),
+      commandName: 'testCommand',
+      reply: jest.fn().mockResolvedValue(undefined),
+    } as unknown as CommandInteraction;
+
+    const mockCommand = {
+      data: { name: 'testCommand' },
+      execute: executeSpy,
+    };
+
+    jest
+      .spyOn(commandsService, 'getCommand')
+      .mockReturnValue(mockCommand as unknown as Command);
+
+    const interactionHandler = getDiscordEventHandler(Events.InteractionCreate);
+    await interactionHandler(interaction);
+
+    expect(interaction.isCommand).toHaveBeenCalled();
+    expect(commandsService.getCommand).toHaveBeenCalledWith('testCommand');
+    expect(executeSpy).toHaveBeenCalledWith(interaction);
+  });
+
+  it('should log an info message on login', async () => {
+    const clientReadyHandler = getDiscordEventHandler(Events.ClientReady);
+    await clientReadyHandler();
+
+    expect(logger.log).toHaveBeenCalledWith('Logged in as test-bot-tag!');
   });
 });

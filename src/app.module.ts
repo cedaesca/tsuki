1c6e1a5d90026b53c465d4555150b82517970b8f
Cesar Escudero <cedaesca@gmail.com>
2024-08-08 19:48:28 -0400

    feat: add env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DiscordModule } from './discord/discord.module';
 import { CommandsModule } from './commands/commands.module';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        BOT_SECRET: Joi.string().required(),
+        BOT_CLIENT_ID: Joi.string().required(),
+        GUILD_ID: Joi.string().required(),
+        REFRESH_COMMANDS_ON_START: Joi.boolean().default(false),
+      }),
     }),
     DiscordModule,
     CommandsModule,

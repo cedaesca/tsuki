e71ea307be9829337c55a3821e8cf8f8a6e68e33
Cesar Escudero <cedaesca@gmail.com>
2024-08-09 10:13:09 -0400

    refactor: move data from property to method

--- src/commands/interfaces/command.interface.ts
+++ src/commands/interfaces/command.interface.ts
@@ -1,6 +1,10 @@
-import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import {
+  CommandInteraction,
+  SlashCommandBuilder,
+  SlashCommandOptionsOnlyBuilder,
+} from 'discord.js';
 
 export interface Command {
-  data: SlashCommandBuilder;
   execute(interaction: CommandInteraction): Promise<void>;
+  getData(): SlashCommandBuilder | SlashCommandOptionsOnlyBuilder;
 }
